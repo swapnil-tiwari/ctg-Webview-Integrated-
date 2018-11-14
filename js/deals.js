bb653c4d9ad6c31f6ebea543b26a4a484da9675f
Swapnil Tiwari <36299079+swapnil-tiwari@users.noreply.github.com>
2018-11-14 21:59:54 +0530

    Home Lock+ Titles Posting etc

--- js/deals.js
+++ js/deals.js
@@ -1,6 +1,6 @@
 function scrapDealForm()
 {
-	return {service:newdeal_serviceType.value,provider:$$client.organisation,status:'ready',shortDescription:newdeal_description.value,longDescription:'the deal',currency:'INR','price':newdeal_price.value}
+	return {service:newdeal_serviceType.value,provider:$$client.organisation,status:'ready',shortDescription:newdeal_title.value,longDescription:newdeal_description.value,currency:'INR','price':newdeal_price.value}
 }
 async function postDeal()
 {
