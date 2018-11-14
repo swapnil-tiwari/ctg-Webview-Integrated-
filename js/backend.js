959e937847493ac6e31fa390c72c034e422e767b
Swapnil Tiwari <36299079+swapnil-tiwari@users.noreply.github.com>
2018-11-14 14:15:31 +0530

    Startup Registration + Deals bug fixes

--- js/backend.js
+++ js/backend.js
@@ -1,241 +1,160 @@
+token = localStorage.getItem('ctg_token')
+async function login() {
+  window.api ? null : window.api = await setAPI();
+  try {
+    debugger;
+    var oldToken = token;
+    token = undefined
+    var response = await api.gettoken.login({
+      virtualID: username.value,
+      password: password.value
+    });
+    if (!response.ok) {
+      token = oldToken;
+    } else {
+      token = response.data;
+      token = localStorage.setItem('ctg_token', token)
+      window.location = './home.html'
+    }
+  } catch (e) {
+    token = oldToken;
+    window.alert("Wrong Credentials");
+    console.log(new Error('CACHED ERROR'),e)
+  }
 
-/*
-async function rpc(proc,arg)
+}
+function scrapRegistrationFields(type)
 {
-	  var req={
-                method:arg?'POST':'GET',
-                headers:{
-                    'content-type': 'application/json'
-                    },
-                }
-	  arg?req.body=JSON.stringify(arg):null;
-    var resp=await fetch('http://localhost:8080/data/'+proc,req);
-    var js=await resp.json();
-    if(js.code>=400)throw js;
-  	return js;
+  if(type=='freelancer')
+  {
+    return {
+      shortName: username_f.value,
+      type: 'freelancer',
+      fullName: username_f.value,
+      firstName: username_f.value,
+      dateRegistered: Date.now(),
+      password: pass_f.value,
+      email: email_f.value,
+      contact: contactno_f.value
+    }
+  }
+  if (type=='startup')
+  {
+    return {
+      shortName: username_sp.value,
+      type: 'startup',
+      fullName: username_sp.value,
+      firstName: username_sp.value,
+      dateRegistered: Date.now(),
+      password: pass_sp.value,
+      email: email_sp.value,
+      contact: contactno_sp.value
+    }
+  }
+  if (type=='individual')
+  {
+    return {
+      shortName: username_ind.value,
+      type: 'individual',
+      fullName: username_ind.value,
+      firstName: username_ind.value,
+      dateRegistered: Date.now(),
+      password: pass_ind.value,
+      email: email_ind.value,
+      contact: contactno_ind.value
+    }
+  }
+}
+async function register(type) {
+  if (!type) throw "type is required"
+  token = undefined;
+  window.api ? null : window.api = await setAPI();
+  try {
+    debugger;
+    var response = await api.gettoken.registerOrg(scrapRegistrationFields(type));
+    localStorage.setItem('ctg_token', response.data);
+    token = response.data;
+    debugger;
+    if (response.ok == true) {
+      await loadForm('completereg' + type);
+      func();
+    }
+  } catch (e) {
+    window.alert("Invalid Details Entered");
+    console.log(new Error('CACHED ERROR'),e)
+  }
+
+}
+// Complete Registration Process Backend Functions
+function scanFields(type) {
+  if(type=='freelancer')
+  {
+    
+  return {
+    communicationlanguage: comlanguage.value,
+    skillslevel: skill,
+    longDescription: description.value,
+    github: github.value,
+    linkedin: linkedin.value,
+    work: work.value,
+    registrationstatus: "completed"
+    }
+  }
+  // if (type=='startup')
+  // {
+  //   return {
+  //     shortName: username_sp.value,
+  //     type: 'startup',
+  //     fullName: username_sp.value,
+  //     firstName: username_sp.value,
+  //     dateRegistered: Date.now(),
+  //     password: pass_sp.value,
+  //     email: email_sp.value,
+  //     contact: contactno_sp.value
+  //   }
+  // }
+}
+async function updateData(type) {
+  if (!type) throw 'type is fiedls';
+  window.api ? null : window.api = await setAPI();
+  var fields=scanFields(type);
+  try {
+  var response = await api.org.updateOrg(fields);
+    debugger;
+    if (response.ok) {
+      window.location.href = '/pages/home.html';
+    }
+
+  } catch (e) {
+    window.alert("Fill the form properly");
+    console.log(new Error('CACHED ERROR'),e)
+  }
+
+}
+async function updateData_s() {
+  window.api ? null : window.api = await setAPI();
+  try {
+    var response = await api["register/update"]({
+      startupname: startupname.value,
+      contact: startupcno.value,
+      country: country.value,
+      description: description.value,
+      currentstatus: currentstatus.value,
+      registrationstatus: "completed"
+    });
+    debugger;
+    if (response.ok) {
+      window.location.href = window.location.origin
+
+    }
+  } catch (e) {
+    window.alert("Fill the form properly")
+    console.log(new Error('CACHED ERROR'),e)
+  }
+
 }
 
-res=await rpc('userdata',['username','email']);
-document.write("<br>");
-document.write(JSON.stringify(res))
-
-
-*/
-
-        // async  function setAPI(cb)
-        // {
-        //   if(window.api)
-        //   {
-        //     cb?cb(window.api):null;
-        //     return window.api
-        //   }
-        //   var loc= localize();
-        //   if(typeof cb=='function')
-        //     {
-
-        //       window.api=await loc
-        //       cb(await loc);
-        //     }
-        //   else
-        //   {
-        //       window.api=await loc
-        //       return await loc;
-        //   }
-        // }
-        token=localStorage.getItem('ctg_token')
-        async function login()
-        {
-            window.api?null:window.api=await setAPI();
-            //await api.login{username:username.value,password:password.value};
-          //   var res=await fetch('http://localhost:8080/data/login',
-          //   {
-          //       method:'post',
-          //       headers:{
-          //           'content-type':'application/json'
-          //       },
-          //       body:JSON.stringify({username:username.value,password:password.value})
-          //   })
-          // //  console.log(JSON.stringify(await res.json()));
-          //   var response= await res.json();
-          try{
-            debugger;
-            var oldToken=token;
-            token=undefined
-            var response= await api.gettoken.login({virtualID:username.value,password:password.value});
-            if (!response.ok)
-            {
-              token=oldToken;
-            }
-            else
-            {
-              token=response.data;
-              token=localStorage.setItem('ctg_token',token)
-              window.location='./home.html'
-            }
-          }
-            catch(e) {
-              token=oldToken;
-               window.alert("Wrong Credentials");
-            }
-
-        }
-        async function register(type)
-        {
-          if(!type)throw "type is required"
-            token=undefined;
-            window.api?null:window.api=await setAPI();
-            // var res=await fetch('http://localhost:8080/data/register',
-            // {
-            //     method:'post',
-            //     headers:{
-            //         'content-type':'application/json'
-            //     },
-            //     body:JSON.stringify({username:username_f.value,password:pass_f.value,email:email_f.value,contact:contactno_f.value})
-            // })
-            try {
-              debugger;
-                  var response=await api.gettoken.registerOrg({shortName:username_f.value,type:'freelancer',fullName:username_f.value,firstName:username_f.value,dateRegistered:Date.now(),password:pass_f.value,email:email_f.value,contact:contactno_f.value});
-                  localStorage.setItem('ctg_token',response.data);
-                  token=response.data;
-                  debugger;
-                  if(response.ok==true)
-                  {
-                    await loadForm('completereg'+type);
-                    func();
-                  }
-            } catch (e) {
-                window.alert("Invalid Details Entered");
-
-
-            }
-            //resp.value=JSON.stringify(await res.json())
-            //console.log(JSON.stringify(await res.json()));
-
-
-        }
-        async function register_sp()
-        {
-          var hash=window.location.hash="startup";
-            window.api?null:window.api=await setAPI();
-            // var res=await fetch('http://localhost:8080/data/register',
-            // {
-            //     method:'post',
-            //     headers:{
-            //         'content-type':'application/json'
-            //     },
-            //     body:JSON.stringify({username:username_sp.value,password:pass_sp.value,email:email_sp.value,contact:contactno_sp.value})
-            // })
-            try {
-                var response=await api.register({username:username_sp.value,password:pass_sp.value,email:email_sp.value,contact:contactno_sp.value});
-                if(response.code==200)
-                {
-                   window.location.href = "../pages/login.html#"+hash;
-                }
-
-            } catch (e) {
-              window.alert("Invalid Details Entered");
-            }
-
-            //resp.value=JSON.stringify(await res.json())
-            //console.log(JSON.stringify(await res.json()));
-
-
-        }
-        async function register_ind()
-        {
-            var hash=window.location.hash="individual";
-            window.api?null:window.api=await setAPI();
-            try {
-                  var response=await api.register({username:username_ind.value,password:pass_ind.value,email:email_ind.value,contact:contactno_ind.value});
-                  if(response.code==200)
-                  {
-                     window.location.href = "../pages/login.html#"+hash;
-                  }
-
-
-
-            } catch (e) {
-                window.alert("Invalid Details Entered");
-            }
-
-
-
-        }
-    // async function logout()
-    // {
-    //     window.api?null:window.api=await setAPI();
-    //     var response=await api.logout();
-    //     if(response.code==200)
-    //     {
-    //       document.location.reload();
-    //     }
-
-
-    // }
-    // async function islogin()
-    // {
-    //     var res=await fetch('/data/islogin',
-    //     {
-    //         method:'get',
-    //         headers:{
-    //             'content-type':'application/json'
-    //         }
-    //     })
-    //     resp.value=JSON.stringify(await res.json())
-    // }
-
-
-    // Complete Registration Process Backend Functions
-    function scanFlFeidls()
-    {
-      return {communicationlanguage:comlanguage.value,skillslevel:skill,longDescription:description.value,github:github.value,linkedin:linkedin.value,work:work.value,registrationstatus:"completed"}
-    }
-    async function updateData(type)
-    {
-      if(!type)throw 'type is fiedls';
-      window.api?null:window.api=await setAPI();
-      try
-      {
-        if(type=='freelancer')
-          var fields=scanFlFeidls();
-        else if(type=='startup')
-          var fields=scanFlFeidls()
-        var response=await api.org.updateOrg(fields);
-        debugger;
-        if (response.ok)
-        {
-           window.location.href ='/pages/home.html';
-        }
-
-      }
-      catch(e)
-      {
-        window.alert("Fill the form properly");
-
-      }
-
-    }
-    async function updateData_s()
-    {
-      window.api?null:window.api=await setAPI();
-      try
-      {
-        var response=await api["register/update"]({startupname:startupname.value,contact:startupcno.value,country:country.value,description:description.value,currentstatus:currentstatus.value,registrationstatus:"completed"});
-        debugger;
-        if (response.ok)
-        {
-           window.location.href =window.location.origin
-
-        }
-      }
-      catch(e)
-      {
-        window.alert("Fill the form properly")
-      }
-
-    }
-function logout()
-{
+function logout() {
   localStorage.clear();
-  window.location='/pages/getting-started.html';
+  window.location = '/pages/getting-started.html';
 }
--- js/fetchClient.js
+++ js/fetchClient.js
@@ -1,23 +1,22 @@
-var $$client=null;
-async function func()
-{
-  window.api?null:window.api=await setAPI();
+var $$client = null;
+async function func() {
+  window.api ? null : window.api = await setAPI();
 
- $$client=$$client||(await api.users.getUser(['firstName','email','contact','id'])).data[0];
+  $$client = $$client || (await api.users.getUser(['firstName', 'email', 'contact', 'id','organisation'])).data[0];
 
- //debugger;
- typeof startup_cap_dash_1!='undefined'?startup_cap_dash_1.innerHTML=$$client.firstName:null;
- typeof startup_cap_dash_2!='undefined'?startup_cap_dash_2.innerHTML=$$client.firstName:null;
+  //debugger;
+  typeof startup_cap_dash_1 != 'undefined' ? startup_cap_dash_1.innerHTML = $$client.firstName : null;
+  typeof startup_cap_dash_2 != 'undefined' ? startup_cap_dash_2.innerHTML = $$client.firstName : null;
 
- typeof startup_cap_post!='undefined'?startup_cap_post.innerHTML=$$client.firstName:null;
- typeof startupname!='undefined'?startupname.value=$$client.firstName:null;
- typeof startupemail!='undefined'?startupemail.value=$$client.email:null;
- typeof startupcno!='undefined'?startupcno.value=$$client.contact:null;
- typeof fname!='undefined'?fname.value=$$client.firstName:null;
- typeof clientname!='undefined'?clientname.innerHTML=$$client.firstName:null;
- typeof clientemail!='undefined'?clientemail.innerHTML=$$client.email:null;
- typeof clientinfo!='undefined'?clientinfo.innerHTML=$$client.description:null;
-  typeof clienttype!='undefined'?clienttype.innerHTML=$$client.type:null;
+  typeof startup_cap_post != 'undefined' ? startup_cap_post.innerHTML = $$client.firstName : null;
+  typeof startupname != 'undefined' ? startupname.value = $$client.firstName : null;
+  typeof startupemail != 'undefined' ? startupemail.value = $$client.email : null;
+  typeof startupcno != 'undefined' ? startupcno.value = $$client.contact : null;
+  typeof fname != 'undefined' ? fname.value = $$client.firstName : null;
+  typeof clientname != 'undefined' ? clientname.innerHTML = $$client.firstName : null;
+  typeof clientemail != 'undefined' ? clientemail.innerHTML = $$client.email : null;
+  typeof clientinfo != 'undefined' ? clientinfo.innerHTML = $$client.description : null;
+  typeof clienttype != 'undefined' ? clienttype.innerHTML = $$client.type : null;
 };
-if(localStorage.ctg_token)token=localStorage.ctg_token
-else if(window.location.search('getting-started.html')<0)window.location='/pages/getting-started.html'
+if (localStorage.ctg_token) token = localStorage.ctg_token
+else if (window.location.search('getting-started.html') < 0) window.location = '/pages/getting-started.html'
